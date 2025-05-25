f8d5ab1ba6f0b1b11d3c3746f5120a8ce0eafc41
louis-henry <48970278+louis-henry@users.noreply.github.com>
2025-05-25 18:01:43 +0200

    [SETUP] Add additional client libraries + appsettings.json

--- Joke.Client/Program.cs
+++ Joke.Client/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
-Console.WriteLine("Hello, World!");
+// Build
+var builder = Host.CreateApplicationBuilder(args);
+
+// Add Configuration
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+    
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+    
+// Add Serilog
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(Log.Logger, dispose: true);
+
+// Add HttpClient
+builder.Services.AddHttpClient();
+
+// Start
+var host = builder.Build();
